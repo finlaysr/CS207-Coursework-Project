1c77cf2222754c65cc77b912d7a4e6b7bfadbe92
Finlay Robb CESM2024 <finlay.robb.2024@uni.strath.ac.uk>
2026-02-26 15:05:25 +0000

    GUI: added input fields and auto cursor moving between text fields

--- Sprint-1/app/src/main/java/org/team25/GUI.java
+++ Sprint-1/app/src/main/java/org/team25/GUI.java
@@ -2,8 +2,10 @@
 package org.team25;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class GUI {
@@ -36,15 +38,6 @@ public class GUI {
     switchContent(new Game());
   }
 
-  class Game extends JPanel {
-    private Game() {
-      this.add(new JLabel("Game"));
-
-      JButton button = new JButton("Undo");
-      this.add(button);
-    }
-  }
-
   protected void switchContent(JPanel pane) {
     // Use invokeLater to ensure thread safety
     if (SwingUtilities.isEventDispatchThread()) {
@@ -57,12 +50,84 @@ public class GUI {
     }
   }
 
-  private static GridBagConstraints setConstraints(int x, int y, int width, int height) {
+  protected static GridBagConstraints setConstraints(int x, int y, int width, int height) {
     GridBagConstraints gbc = new GridBagConstraints();
     gbc.gridx = x;
     gbc.gridy = y;
     gbc.gridwidth = width;
     gbc.gridheight = height;
     return gbc;
+  }
+}
+
+class Game extends JPanel {
+
+  private JPanel inputGroup;
+  private ArrayList<JTextField> inputFields;
+
+  protected Game() {
+    this.inputGroup = new JPanel(new FlowLayout());
+    this.setLayout(new GridBagLayout());
+    this.inputFields = new ArrayList<>();
+
+    this.add(new JLabel("Game"), GUI.setConstraints(0, 0, 1, 1));
+
+    JButton button = new JButton("Undo");
+    this.add(button, GUI.setConstraints(0, 1, 1, 1));
+
+    this.add(inputGroup, GUI.setConstraints(0, 2, 1, 1));
+
+    ArrayList<String> words = new ArrayList<>();
+    words.add("Hello");
+    words.add("World");
+    words.add("Test");
+    addWords(words);
+  }
+
+  protected void addWords(ArrayList<String> words) {
+    int i = -1;
+    for (String word : words) {
+      JPanel wordPanel = new JPanel(new FlowLayout());
+      wordPanel.setBackground(Color.GRAY);
+      for (char c : word.toCharArray()) {
+        i++;
+        JPanel charPanel = new JPanel(new GridBagLayout());
+        JTextField textField = new JTextField(1);
+        int finalI = i;
+        textField.addKeyListener(
+            new java.awt.event.KeyAdapter() {
+              @Override
+              public void keyTyped(java.awt.event.KeyEvent evt) {
+                if (evt.getKeyChar() == KeyEvent.VK_DELETE
+                    || evt.getKeyChar() == KeyEvent.VK_BACK_SPACE) {
+                  textField.setText("");
+                  if (finalI > 0) {
+                    inputFields.get(finalI - 1).requestFocus();
+                  }
+                } else {
+                  textField.setText(Character.toString(evt.getKeyChar()));
+                  if (finalI < inputFields.size() - 1) {
+                    inputFields.get(finalI + 1).requestFocus();
+                  }
+                }
+                evt.consume();
+              }
+            });
+        textField.addFocusListener(
+            new java.awt.event.FocusAdapter() {
+              @Override
+              public void focusGained(java.awt.event.FocusEvent evt) {
+                inputFields.forEach(field -> field.setBackground(Color.WHITE));
+                textField.setBackground(Color.YELLOW);
+              }
+            });
+        inputFields.add(textField);
+        charPanel.add(textField, GUI.setConstraints(0, 0, 1, 1));
+
+        charPanel.add(new JLabel(Character.toString(c)), GUI.setConstraints(0, 1, 1, 1));
+        wordPanel.add(charPanel);
+      }
+      inputGroup.add(wordPanel);
+    }
   }
 }
