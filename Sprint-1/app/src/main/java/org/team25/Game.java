51e16eeaf882eecec5a574bd4271423eaba61e1e
callum <callum.charteris.2024@uni.strath.ac.uk>
2026-03-05 11:35:29 +0000

    Game class development

--- Sprint-1/app/src/main/java/org/team25/Game.java
+++ Sprint-1/app/src/main/java/org/team25/Game.java
@@ -1,12 +1,25 @@
 /* CS207 Cryptogram Project - Sprint 1 - Team 25 2026 */
 package org.team25;
 
+/*Imports Required */
 import java.util.Scanner;
 
+/** This is the game class which acts as the controller/engine for the overall system */
 public class Game {
 
+  // instance variables- still to decdie the data types of the variables
+  // private payerGameMapping; //need to decide a data type
+  private Player currentPlayer;
+
+  // constructor for Game, empty for now
+  public Game() {}
+
+  /** User story 1: As a player I want to be able to generate a cryptogram */
   private void generateCryptogram() {}
 
+  /**
+   * User story 2: As a player I want to be able to enter a letter so I can solve the cryptogram.
+   */
   public void enterLetter() {
     boolean validGuess = false;
     while (!validGuess) {
@@ -22,5 +35,6 @@ public class Game {
     }
   }
 
+  /** User story 3: As a player I want to be able to undo a letter so I can play the cryptogram */
   public void undoLetter() {}
 }
